1f8c67da73598b95f964db36a54ef269a2143421
Loan Tran <loantranvn810@gmail.com>
2018-09-04 14:13:55 -0500

    Add styling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,4 +1,22 @@
 // Check off specific todos by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
     $(this).toggleClass("completed");     
+}); 
+
+// Click on X to delete a task
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove(); 
+    }); 
+    event.stopPropagation(); // stop event bubbling 
+}); 
+
+$("input[type='text']").keypress(function(event) {
+    if (event.which === 13) {
+        // grabbing new todo text from input
+        var todoText = $(this).val(); 
+        $(this).val(""); 
+        // create a new li and add to ul
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>"); 
+    }
 }); 
